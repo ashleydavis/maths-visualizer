115a139fcff7a8c321d32fa55906388dee998e40
Ashley Davis <ashley@codecapers.com.au>
2018-12-14 20:30:45 +1000

    Added an example network. Think I might use this to represent the graph of possible transformations for an equation.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import MathJax from 'react-mathjax-preview';
+import Graph from 'react-graph-vis';
 
 export interface IAppProps {
 }
@@ -11,6 +12,44 @@ export interface IMathsFormula {
     title: string;
     expr: string;   
 }
+
+const graph = {
+    nodes: [
+        {id: 1, label: 'Node 1'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'},
+        {id: 4, label: 'Node 4'},
+        {id: 5, label: 'Node 5'}
+      ],
+    edges: [
+        {from: 1, to: 2},
+        {from: 1, to: 3},
+        {from: 2, to: 4},
+        {from: 2, to: 5}
+      ]
+  };
+  
+    const options = {
+        width: '600px',
+        height: '300px',
+        layout: {
+            improvedLayout:true,
+            hierarchical: {
+                enabled: true,
+                direction: "LR",
+            }
+      },
+      edges: {
+          color: "black"
+      }
+  };
+  
+  const events = {
+      select: (event: any) => {
+          var { nodes, edges } = event;
+          console.log(event);   
+      }
+  }
 
 export class AppUI extends React.Component<IAppProps, IAppState> {
     
@@ -32,7 +71,13 @@ export class AppUI extends React.Component<IAppProps, IAppState> {
                         <div>{index+1} {formula.title}</div>
                         <MathJax math={formula.expr} />
                     </div>
-                )}
+                )}  
+                <div>
+                    <Graph 
+                        style={{ border: "1px solid gray", }}
+                        graph={graph} options={options} events={events} 
+                        />
+                </div>
             </div>
         );
     }
--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,1 +1,2 @@
 declare module 'react-mathjax-preview';
+declare module 'react-graph-vis';
